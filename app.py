5706333094fec8ccec2965cf2e6a8133dafbacd7
Garrett Moody <extrillieos@gmail.com>
2022-10-17 12:55:54 -0700

    token decorator

--- app.py
+++ app.py
@@ -1,16 +1,77 @@
-from flask import Flask
- 
-# Flask constructor takes the name of
-# current module (__name__) as argument.
+from functools import wraps
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+import os
+import uuid
+import jwt
+
+load_dotenv()
+
+
+
 app = Flask(__name__)
- 
+app.config['SECRET_KEY'] = str(os.environ.get("SECRET_KEY"))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    public_id = db.Column(db.String(50), unique = True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(70), unique = True)
+
+# Decorator for validating a JWT token
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        # jwt is passed in the request header
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+
+        if not token:
+            return jsonify({'message': 'Missing Access Token!'}), 401
+
+        try:
+            # decoding the payload to fetch the stored details
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            current_user = User.query\
+                .filter_by(public_id=data['public_id'])\
+                .first()
+        except:
+            return jsonify({
+                'message': 'Token is invalid !!'
+            }), 401
+        # returns the current logged in users contex to the routes
+        return f(current_user, *args, **kwargs)
+
+    return decorated
+
+# Endpoint for SSO login
+@app.route('/login', methods =['POST'])
+def login():
+    #SSO Redirect Logic
+    pass
+
+@app.route("/login/callback")
+def callback():
+    # Handle SSO callback
+    code = request.args.get("code")
+
+
+
+
 # The route() function of the Flask class is a decorator,
 # which tells the application which URL should call
 # the associated function.
 @app.route('/')
 def hello_world():
-    return 'Hello World'
- 
+    return 'Hello World!'
+
 
 # main driver function
 if __name__ == '__main__':
