0f515436b8bf79ef4bddd03aaa5ba43763182fd8
haydentinker <haydentinker613@gmail.com>
2022-10-25 09:57:59 -0700

    Create test_api.py
    Adding a file for our tests for the api

---
+++ test_api.py
@@ -0,0 +1,9 @@
+from app import app
+import pytest
+
+def test_index_route():
+    response = app.test_client().get('/unprotected')
+
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == 'No Token No problem!'
+    
