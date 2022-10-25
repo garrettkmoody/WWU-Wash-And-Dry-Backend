e299132b9661b6b660bb345f059bbfbbb5ad0063
haydentinker <haydentinker613@gmail.com>
2022-10-25 09:59:12 -0700

    Updating test_api.py
    Changing function name in test_api.py

--- test_api.py
+++ test_api.py
@@ -1,9 +1,8 @@
 from app import app
 import pytest
 
-def test_index_route():
+def test_unprotected_route():
     response = app.test_client().get('/unprotected')
 
     assert response.status_code == 200
     assert response.data.decode('utf-8') == 'No Token No problem!'
-    
