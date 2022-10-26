4847eccdaa99e93ce4ec8e447136a68b5a404749
TaylorSmith28 <tjs6282000@hotmail.com>
2022-10-25 21:07:38 -0700

    Added Machine Class

--- app.py
+++ app.py
@@ -18,12 +18,20 @@ app.config['AZURE_OAUTH_APPLICATION_ID'] = str(
     os.environ.get("APPLICATION_ID"))
 db = SQLAlchemy(app)
 
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     public_id = db.Column(db.String(50), unique=True)
     name = db.Column(db.String(100))
     email = db.Column(db.String(70), unique=True)
+
+class Machine(db.Model):
+    id = db.Column(db.Integer, primary_Key=True)
+    public_id = db.Column(db.String(50), unique=True)
+    dorm = db.Column(db.String(10))
+    floor = db.Column(db.Integer)
+    is_available = db.Column(db.Bool)
+    last_service_date = db.Column(db.Integer)
+    installation_date = db.Column(db.Integer)
 
 with app.app_context():
     db.create_all()
