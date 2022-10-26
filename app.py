a8238881e6e3b95cc7bd9734f1e5dc9562887729
haydentinker <haydentinker613@gmail.com>
2022-10-26 13:35:56 -0700

    Adding Endpoints/tests and updating requirements.txt along with gitignore

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import datetime
 from functools import wraps
-from flask import Flask, redirect, request, jsonify
+from flask import Flask, render_template, redirect, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import jwt
@@ -10,23 +10,44 @@ import requests
 load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = str(os.environ.get("SECRET_KEY"))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Database.dbUser'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['AZURE_OAUTH_TENANCY'] = str(os.environ.get("TENANT_ID"))
 app.config['AZURE_OAUTH_CLIENT_SECRET'] = str(os.environ.get("CLIENT_SECRET"))
 app.config['AZURE_OAUTH_APPLICATION_ID'] = str(
     os.environ.get("APPLICATION_ID"))
-db = SQLAlchemy(app)
 
+#This might not need to be split
+dbUser = SQLAlchemy(app)
+dbMachines = SQLAlchemy(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    public_id = db.Column(db.String(50), unique=True)
-    name = db.Column(db.String(100))
-    email = db.Column(db.String(70), unique=True)
+class User(dbUser.Model):
+    #The tablename may be unnecessary 
+    __tablename__ = 'user'
+    id = dbUser.Column(dbUser.Integer, primary_key=True)
+    public_id = dbUser.Column(dbUser.String(50), unique=True)
+    name = dbUser.Column(dbUser.String(100))
+    email = dbUser.Column(dbUser.String(70), unique=True)
 
 with app.app_context():
-    db.create_all()
+    dbUser.create_all()
+
+class Machines(dbMachines.Model):
+    __tablename__ = 'machines'
+    #Unique id for each machine
+    id = dbMachines.Column(dbMachines.Integer, primary_key=True)
+    #An id based on floor
+    floor_id = dbMachines.Column(dbMachines.String(50), unique=True)
+    dorm = dbMachines.Column(dbMachines.String(10))
+    floor = dbMachines.Column(dbMachines.Integer)
+    is_available = dbMachines.Column(dbMachines.Boolean)
+    last_service_date = dbMachines.Column(dbMachines.String)
+    installation_date = dbMachines.Column(dbMachines.String)
+
+with app.app_context():
+    dbMachines.create_all()
+
+
 # Token Function Decorator
 
 
@@ -77,8 +98,8 @@ def callback():
         userData = userResponse.json()
         if not User.query.filter_by(public_id=userData['id']).first():
             newUser = User(public_id=userData['id'], name=userData['displayName'], email=userData['mail'])
-            db.session.add(newUser)
-            db.session.commit()
+            dbUser.session.add(newUser)
+            dbUser.session.commit()
             print("New User Created!")
 
         token = jwt.encode({'public_id': userData['id'], 'exp': datetime.datetime.utcnow(
@@ -86,6 +107,29 @@ def callback():
         return redirect("http://www.google.com?token=" + token)
     except:
         return redirect("http://www.google.com?error=AuthFailed")
+
+
+@app.route('/machines/<int:requested_id>', methods=['GET'])
+def get_machine_by_id(requested_id):
+    machine= dbMachines.one_or_404(dbMachines.select(Machines).filter_by(id == requested_id)).description = f"Machine does not exist with the id '{requested_id}'."
+    return render_template("show_machine", machine=machine)
+
+@app.route('/machines/<string:requested_dorm>/<int:requested_floor>/<int:requested_floor_id>', methods=['GET'])
+def get_machine_by_dorm_floor_floorid(requested_dorm, requested_floor, requested_floor_id):
+    #May need better error handling if they request Sittner with an non-existent floor number / floor_id
+    machine= dbMachines.one_or_404(dbMachines.select(Machines).filter_by(Machines.id == requested_floor_id, Machines.floor == requested_floor, Machines.dorm == requested_dorm)).description=f"Either requested dorm '{requested_dorm}' does not exist, requested_floor '{requested_floor}' does not exist, requested_floor_id '{requested_floor_id}' does not exist."
+    return render_template("show_machine", machine = machine)
+
+@app.route('/machines/<string:requested_dorm>/<int:requested_floor>', methods=['GET'])
+def get_machines_by_dorm_and_floor(requested_dorm, requested_floor):
+    #May need better error handling if they request Sittner with an non-existent floor number
+    machine= dbMachines.get_or_404(dbMachines.select(Machines).filter_by(Machines.floor == requested_floor, Machines.dorm == requested_dorm)).description=f"Either requested dorm '{requested_dorm}' does not exist, requested_floor '{requested_floor}' does not exist."
+    return render_template("show_machine", machine = machine)
+
+@app.route('/machines/<string:requested_dorm>', methods=['GET'])
+def get_machines_by_dorm( requested_dorm):
+    machine= dbMachines.get_or_404(dbMachines.select(Machines).filter_by(Machines.dorm == requested_dorm)).description=f"Requested dorm '{requested_dorm}' does not exist."
+    return render_template("show_machine", machine = machine)
 
 
 # Unprotected route, no token required
