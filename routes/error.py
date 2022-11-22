f26077cd7aa2d95825a95c679c2b545dc12f6235
Taylor Smith <tjs6282000@hotmail.com>
2022-11-22 01:05:41 -0500

    Fixed previous pylint errors

--- routes/error.py
+++ routes/error.py
@@ -7,86 +7,86 @@ from flask import Blueprint, jsonify, abort
 error = Blueprint('error',__name__)
 
 @error.errorhandler(400)
-def custom_400_errorhandler(error):
+def custom_400_errorhandler(error_message):
     """
     Error handler for status code 400 which is a bad request
     Parameters: Takes in an error
     Returns: A json message saying what the error is and the error code
     """
-    return jsonify(error=str(error)), 400
+    return jsonify(error=str(error_message)), 400
 
 @error.errorhandler(401)
-def custom_401_errorhandler(error):
+def custom_401_errorhandler(error_message):
     """
     Error handler for status code 401 which is a unauthorized user error
     Parameters: Takes in an error
     Returns: A json message saying what the error is and the error code
     """
-    return jsonify(error=str(error)), 401
+    return jsonify(error=str(error_message)), 401
 
 @error.errorhandler(403)
-def custom_403_errorhandler(error):
+def custom_403_errorhandler(error_message):
     """
     Error handler for status code 403 which is a Forbidden API error
     Parameters: Takes in an error
     Returns: A json message saying what the error is and the error code
     """
-    return jsonify(error=str(error)), 403
+    return jsonify(error=str(error_message)), 403
 
 @error.errorhandler(404)
-def custom_404_errorhandler(error):
+def custom_404_errorhandler(error_message):
     """
     Error handler for status code 404 which is not found error
     Parameters: Takes in an error
     Returns: A json message saying what the error is and the error code
     """
-    return jsonify(error=str(error)), 404
+    return jsonify(error=str(error_message)), 404
 
 @error.errorhandler(429)
-def custom_429_errorhandler(error):
+def custom_429_errorhandler(error_message):
     """
     Error handler for status code 429 which indicates to many requests
     have been made at this time
     Parameters: Takes in an error
     Returns: A json message saying what the error is and the error code
     """
-    return jsonify(error=str(error)), 429
+    return jsonify(error=str(error_message)), 429
 
 @error.errorhandler(500)
-def custom_500_errorhandler(error):
+def custom_500_errorhandler(error_message):
     """
     Error handler for status code 500 which is a internal server error
     Parameters: Takes in an error
     Returns: A json message saying what the error is and the error code
     """
-    return jsonify(error=str(error)), 500
+    return jsonify(error=str(error_message)), 500
 
 @error.errorhandler(502)
-def custom_502_errorhandler(error):
+def custom_502_errorhandler(error_message):
     """
     Error handler for status code 502 which is a bad gateway error
     Parameters: Takes in an error
     Returns: A json message saying what the error is and the error code
     """
-    return jsonify(error=str(error)), 502
+    return jsonify(error=str(error_message)), 502
 
 @error.errorhandler(503)
-def custom_503_errorhandler(error):
+def custom_503_errorhandler(error_message):
     """
     Error handler for status code 503 which is a service unavailable error
     Parameters: Takes in an error
     Returns: A json message saying what the error is and the error code
     """
-    return jsonify(error=str(error)), 503
+    return jsonify(error=str(error_message)), 503
 
 @error.errorhandler(504)
-def custom_504_errorhandler(error):
+def custom_504_errorhandler(error_message):
     """
     Error handler for status code 504 which indicates the gateway has timed out
     Parameters: Takes in an error
     Returns: A json message saying what the error is and the error code
     """
-    return jsonify(error=str(error)), 504
+    return jsonify(error=str(error_message)), 504
 
 
 #-----------------------------Routes to test custom error templates-------------------
